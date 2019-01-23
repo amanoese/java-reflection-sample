6ca863bfee599fc27fda3a1540a474de7db7d4c9
Seito Taka <amanoese@gmail.com>
2019-01-23 09:48:18 +0900

    fix code to simplify

--- src/test/java/EnvironmentTest.java
+++ src/test/java/EnvironmentTest.java
@@ -1,6 +1,5 @@
 import org.junit.Test;
 
-import java.lang.reflect.Modifier;
 import java.lang.reflect.Field;
 import java.util.Map;
 
@@ -11,18 +10,13 @@ public class EnvironmentTest {
   public void replaceEnvironment() throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
 
 	//Final Classのメソッドを取得しアクセス可能に
-	Field theCaseInsensitiveEnvironment =  Class.forName("java.lang.ProcessEnvironment").getDeclaredField("theCaseInsensitiveEnvironment");
+	Class<?> clazz = Class.forName("java.lang.ProcessEnvironment");
+    Field theCaseInsensitiveEnvironment = clazz.getDeclaredField("theCaseInsensitiveEnvironment");
 	theCaseInsensitiveEnvironment.setAccessible(true);
 
-	//リフレクションでもデフォルトではPrivateまたはFinalなものは書き換えは禁止されているので可能な状態に変更
-	Field field = Field.class.getDeclaredField("modifiers");
-	field.setAccessible(true);
-	field.setInt(theCaseInsensitiveEnvironment,theCaseInsensitiveEnvironment.getModifiers() & ~Modifier.PRIVATE & ~Modifier.FINAL);
-
 	//システム環境変数で必要なものだけを差し替える
-	Map<String,String> seytemEnv = (Map<String, String>) theCaseInsensitiveEnvironment.get(null);
-	seytemEnv.put("TEMP","/MY_TEMP_DIR");
-	theCaseInsensitiveEnvironment.set(null,seytemEnv);
+	Map<String,String> sytemEnviroment = (Map<String, String>) theCaseInsensitiveEnvironment.get(null);
+    sytemEnviroment.put("TEMP","/MY_TEMP_DIR");
 
 	assertEquals(System.getenv("TEMP"),"/MY_TEMP_DIR");
   }
